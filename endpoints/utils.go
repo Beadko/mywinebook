aaf41503dfb99c80d871a54b90e481f77dfeabdf
Beadko <beadko@gmail.com>
2024-06-01 06:17:27 +0700

    Create CRUD commands WIP

--- endpoints/utils.go
+++ endpoints/utils.go
@@ -3,7 +3,6 @@ package endpoints
 import (
 	"encoding/json"
 	"fmt"
-	"log"
 	"net/http"
 
 	"github.com/Beadko/mywinebook/data"
@@ -12,15 +11,19 @@ import (
 )
 
 // AddRouterEndpoints add the actual endpoints for api
+
 func AddRouterEndpoints(r *mux.Router) *mux.Router {
-	r.HandleFunc("/api/wine", getWine).Methods("GET")
-	r.HandleFunc("/api/wine", addWine).Methods("POST")
-	r.HandleFunc("/api/wine/{WINE_ID}", deleteWine).Methods("DELETE")
-	r.HandleFunc("/api/wine/{WINE_ID}", updateWine).Methods("PUT")
+	home := homeHandler{}
+	r.HandleFunc("/", home.ServeHTTP)
+	r.HandleFunc("/wine", getWines).Methods("GET")
+	r.HandleFunc("/wine/{id}", getWine).Methods("GET")
+	r.HandleFunc("/wine", addWine).Methods("POST")
+	r.HandleFunc("/wine/{id}", deleteWine).Methods("DELETE")
+	// r.HandleFunc("/api/wine/{WINE_ID}", updateWine).Methods("PUT")
 	return r
 }
 
-func sendJSONResponse(w http.ResponseWriter, data interface{}) {
+/* func sendJSONResponse(w http.ResponseWriter, data interface{}) {
 	body, err := json.Marshal(data)
 	if err != nil {
 		log.Printf("Failed to encode a JSON response: %v", err)
@@ -34,29 +37,63 @@ func sendJSONResponse(w http.ResponseWriter, data interface{}) {
 		log.Printf("Failed to write the response body: %v", err)
 		return
 	}
+}*/
+
+type homeHandler struct{}
+
+func (h *homeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("My wine book homepage"))
+}
+
+func getWines(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("My wine list"))
+	resp, err := data.GetWines()
+	if err != nil {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
 }
 
 func getWine(w http.ResponseWriter, r *http.Request) {
-	data.DisplayAllNotes()
-	return
+	w.Write([]byte("My wine"))
+	id := mux.Vars(r)["id"]
+	resp, err := data.GetWine(id)
+	if err != nil {
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(resp)
 }
 
-func updateWine(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func deleteWine(w http.ResponseWriter, r *http.Request) {
-
-}
-
-func addWine(w http.ResponseWriter, r *http.Request) {
+/*func updateWine(w http.ResponseWriter, r *http.Request) {
 	var wine wine.Wine
 	json.NewDecoder(r.Body).Decode(&wine)
-	err := data.InsertNote(wine.Name, string(wine.Type))
+	data.UpdateNote(wine.Name)
+}*/
+
+func deleteWine(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	err := data.DeleteWine(id)
 	if err != nil {
-		http.Error(w, "Failed to create user", http.StatusInternalServerError)
+		http.Error(w, "Failed to delete a note", http.StatusInternalServerError)
 		return
 	}
 	w.WriteHeader(http.StatusCreated)
-	fmt.Fprintln(w, "User created successfully")
+	fmt.Fprintln(w, "Note deleted successfully")
+}
+
+func addWine(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Add wine"))
+	var wine wine.Wine
+	if err := json.NewDecoder(r.Body).Decode(&wine); err != nil {
+		return
+	}
+	err := data.AddWine(wine.Name, string(wine.Type))
+	if err != nil {
+		http.Error(w, "Failed to add a note", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprintln(w, "Note created successfully")
 }
