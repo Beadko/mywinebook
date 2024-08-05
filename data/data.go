9a0cbf67b0fae9a28150207ee5b11c9932d3b9db
Beadko <beadko@gmail.com>
2024-08-05 21:05:46 +0700

    Add the remaining attributes to the wines

--- data/data.go
+++ data/data.go
@@ -24,24 +24,124 @@ func InitDB() {
 	db.Exec(`
 		CREATE TABLE IF NOT EXISTS wine_types (
 			id INTEGER PRIMARY KEY AUTOINCREMENT,
-			name TEXT UNIQUE
+			name VARCHAR(100) UNIQUE
 			);
-			INSERT INTO wine_types(name) VALUES ("Red"), ("White"), ("Rose"), ("Sparkling");`)
+			INSERT INTO wine_types(name) VALUES ("Red"), ("White"), ("Rose"), ("Sparkling"), ("Orange");`)
 	db.Exec(`
 		CREATE TABLE IF NOT EXISTS countries (
-		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		name TEXT UNIQUE
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE
 		);
-		INSERT INTO countries(name) VALUES ("France"), ("Italy"), ("Australia");`)
+		INSERT INTO countries(name) VALUES ("France"), ("Italy"), ("Australia"), ("Spain"), ("New Zealand"), ("Chile"), ("Germany"), ("Malta"), ("USA"), ("Argentina"), ("South Africa"), ("Portugal"), ("Hungary"), ("Georgia");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS aromas (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE
+		);
+		INSERT INTO aromas(name) VALUES ("Fruity"), ("Vegetal"), ("Floral"), ("Earthy"), ("Woody"), ("Spicy"), ("Mineral");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS intensities (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO instensities(name) VALUES ("Weak"), ("Medium"), ("Pronounced");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS flavours (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO flavours(name) VALUES ("Fruity"), ("Vegetal"), ("Floral"), ("Earthy"), ("Woody"), ("Spicy"), ("Mineral"), ("Herbal");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS sweetness (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO sweetness(name) VALUES ("Bone dry"), ("Dry"), ("Off dry"), ("Medium sweet"), ("Sweet"), ("Very sweet");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS acidities (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO acidities(name) VALUES ("Tart"), ("Crisp"), ("Fresh"), ("Smooth"), ("Not acidic");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS tannins (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO tannins(name) VALUES ("Very light"), ("Light"), ("Medium"), ("Full Bodied"), ("Heavy");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS bodies (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO bodies(name) VALUES ("Soft"), ("Round"), ("Dry"), ("Hard");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS clarities (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO clarities(name) VALUES ("Clear"), ("Slightly hazy"), ("Hazy");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS finishes (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO finishes(name) VALUES ("Short"), ("Medium"), ("Long"), ("Very long");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS colours (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO colours(name) VALUES ("Purple"), ("Ruby"), ("Garnet"), ("Tawny"), ("Straw"), ("Yellow"), ("Golden"), ("Blush"), ("Salmon"), ("Pink");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS colour_depths (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO colour_depths(name) VALUES ("Pale"), ("Medium"), ("Deep");`)
+	db.Exec(`
+		CREATE TABLE IF NOT EXISTS balances (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			name VARCHAR(100) UNIQUE		
+		);
+		INSERT INTO balances(name) VALUES ("Good"), ("Fair"), ("Unbalanced");`)
 	db.Exec(`
 		CREATE TABLE IF NOT EXISTS wines (
 			id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-			name TEXT,
+			name VARCHAR(100),
 			wineType INT unsigned,
 			country INT unsigned,
-			score INT,
+			score INT unsigned,
+			year INT unsigned,
+			producer VARCHAR(100),
+			alcohol DECIMAL(4, 2),
+			aroma INT unsigned,
+			intensity INT unsigned,
+			palate INT unsigned,
+			flavour INT unsigned,
+			sweetness INT unsigned,
+			acidity INT unsigned,
+			tannin INT unsigned,
+			body INT unsigned,
+			clarity INT unsigned,
+			finish INT unsigned,
+			colour INT unsigned,
+			colour_depth INT unsigned,
+			balance INT unsigned,
 			FOREIGN KEY (wineType) REFERENCES wine_types(id),
-			FOREIGN KEY (country) REFERENCES countries(id)
+			FOREIGN KEY (country) REFERENCES countries(id),
+			FOREIGN KEY (aroma) REFERENCES aromas(id),
+			FOREIGN KEY (instensity) REFERENCES intensities(id),
+			FOREIGN KEY (flavour) REFERENCES flavours(id),
+			FOREIGN KEY (sweetness) REFERENCES sweetness(id),
+			FOREIGN KEY (acidity) REFERENCES acidities(id),
+			FOREIGN KEY (tannin) REFERENCES tannins(id),
+			FOREIGN KEY (body) REFERENCES bodies(id),
+			FOREIGN KEY (clarity) REFERENCES clarities(id),
+			FOREIGN KEY (finish) REFERENCES finishes(id),
+			FOREIGN KEY (colour) REFERENCES colours(id)
+			FOREIGN KEY (colour_depth) REFERENCES colour_depths(id)
+			FOREIGN KEY (balance) REFERENCES balances(id)
 	);`)
 
 	log.Println("Database created")
--- internal/wine/wine.go
+++ internal/wine/wine.go
@@ -1,19 +1,32 @@
 package wine
 
-import "github.com/guregu/null/v5/zero"
+import (
+	"time"
+
+	"github.com/guregu/null/v5/zero"
+)
 
 type Wine struct {
-	ID        int      `json:"id"`
-	TypeID    zero.Int `json:"wine_type"`
-	Name      string   `json:"name"`
-	CountryID zero.Int `json:"country"`
-	Score     zero.Int `json:"score"`
-	/*Producer string    `json:"producer"`
-	Year     time.Time `json:"year"`
-	Alcohol  float64   `json:"alcohol"`
-	Nose     Nose      `json:"nose"`
-	Palate   Palate    `json:"palate"`
-	Colour   Colour    `json:"colour"`*/
+	ID          int       `json:"id"`
+	TypeID      zero.Int  `json:"wine_type"`
+	Name        string    `json:"name"`
+	CountryID   zero.Int  `json:"country"`
+	Score       zero.Int  `json:"score"`
+	Producer    string    `json:"producer"`
+	Year        time.Time `json:"year"`
+	Alcohol     float64   `json:"alcohol"`
+	AromaID     zero.Int  `json:"aroma"`
+	IntensityID zero.Int  `json:"intensity"`
+	FlavourID   zero.Int  `json:"flavour"`
+	SweetnessID zero.Int  `json:"sweetness"`
+	AcidityID   zero.Int  `json:"acidity"`
+	TanninID    zero.Int  `json:"tannin"`
+	BodyID      zero.Int  `json:"body"`
+	ClarityID   zero.Int  `json:"clarity"`
+	Finish      zero.Int  `json:"finish"`
+	ColourID    zero.Int  `json:"colour"`
+	DepthID     zero.Int  `json:"colour_depth"`
+	BalanceID   zero.Int  `json:"balance"`
 }
 type WineType struct {
 	ID   int    `json:"id"`
@@ -25,158 +38,57 @@ type Country struct {
 	Name string `json:"name"`
 }
 
-type Colour struct {
-	Red     RedColour
-	White   WhiteColour
-	Rose    RoseColour
-	Clarity Clarity
-}
-type RedColour string
-
-// Public Red
-const (
-	Purple RedColour = "purple"
-	Ruby   RedColour = "ruby"
-	Garnet RedColour = "garnet"
-	Tawny  RedColour = "tawny"
-)
-
-type WhiteColour string
-
-// Public White
-const (
-	Straw  WhiteColour = "straw"
-	Yellow WhiteColour = "yellow"
-	Golden WhiteColour = "golden"
-)
-
-type RoseColour string
-
-// Public Rose
-const (
-	Blush  RoseColour = "blush"
-	Salmon RoseColour = "salmon"
-	Pink   RoseColour = "pink"
-)
-
-type Clarity string
-
-// Public Clarity
-const (
-	Clear        Clarity = "clear"
-	SlightlyHazy Clarity = "slightly_hazy"
-	Hazy         Clarity = "hazy"
-)
-
-type Palate struct {
-	Sweetness Sweetness
-	Acidity   Acidity
-	Tannin    Tannin
-	Body      Body
-	Finish    Finish
-	Balance   Balance
-	flavours  []Flavour
-}
-type Sweetness string
-
-// Public Sweetness
-const (
-	BoneDry     Sweetness = "bone_dry"
-	Dry         Sweetness = "dry"
-	OffDry      Sweetness = "off_dry"
-	MediumSweet Sweetness = "medium_sweet"
-	Sweet       Sweetness = "sweet"
-	VerySweet   Sweetness = "very_sweet"
-)
-
-type Acidity string
-
-// Public Acidity
-const (
-	Tart      Acidity = "tart"
-	Crisp     Acidity = "crisp"
-	Fresh     Acidity = "fresh"
-	Smooth    Acidity = "smooth"
-	NotAcidic Acidity = "non_acidic"
-)
-
-type Tannin string
-
-// Public Tannin
-const (
-	Soft  Tannin = "tannin"
-	Round Tannin = "round"
-	Mouth Tannin = "dry_mouth"
-	Hard  Tannin = "hard"
-)
-
-type Body string
-
-// Public Body
-const (
-	VeryLight  Body = "very_light"
-	Light      Body = "light"
-	Medium     Body = "medium"
-	FullBodied Body = "full_bodied"
-	Heavy      Body = "heavy"
-)
-
-type Finish string
-
-// Public Finish
-const (
-	Short    Finish = "short"
-	Middle   Finish = "middle"
-	Long     Finish = "long"
-	VeryLong Finish = "very_long"
-)
-
-type Balance string
-
-// Public Balance
-const (
-	Good       Balance = "good"
-	Fair       Balance = "fair"
-	Unbalanced Balance = "unbalanced"
-)
-
-type Flavour string
-
-// Public Flavour
-const (
-	Fruity  Flavour = "fruity"
-	Vegetal Flavour = "vegetal"
-	Floral  Flavour = "floral"
-	Earthy  Flavour = "earthy"
-	Woody   Flavour = "woody"
-	Herbal  Flavour = "herbal"
-	Spicy   Flavour = "spicy"
-	Mineral Flavour = "mineral"
-)
-
-type Nose struct {
-	aromas    []Aroma
-	Intensity Intensity
+type Aroma struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
 }
 
-type Aroma string
+type Intensity struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
-// Public Aroma
-const (
-	FruityAroma  Aroma = "fruity"
-	VegetalAroma Aroma = "vegetal"
-	FloralAroma  Aroma = "flowers"
-	EarthyAroma  Aroma = "earth"
-	WoodyAroma   Aroma = "woody"
-	SpicyAroma   Aroma = "spicy"
-	MineralAroma Aroma = "mineral"
-)
+type Flavour struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
-type Intensity string
+type Sweetness struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
 
-// Public Intensity
-const (
-	WeakIntensity       Intensity = "weak"
-	MediumIntensity     Intensity = "medium"
-	PronouncedIntensity Intensity = "pronounced"
-)
+type Acidity struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Tannin struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Body struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Clarity struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Finish struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Depth struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+type Balance struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
