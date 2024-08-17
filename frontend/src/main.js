3612dbe0d2a91a92949112ddc6098ed0b6a9e7a5
Beadko <beadko@gmail.com>
2024-08-17 12:48:42 +0700

    Add componets

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,8 +8,11 @@ import DataTable from 'primevue/datatable'
 import Button from 'primevue/button'
 import Select from 'primevue/select'
 import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
 import Dialog from 'primevue/dialog'
 import Rating from 'primevue/rating'
+import Panel from 'primevue/panel'
+import RadioButton from 'primevue/radiobutton'
 import App from './App.vue'
 
 
@@ -24,8 +27,10 @@ app.component('DataTable', DataTable)
 app.component('Button', Button)
 app.component('Select', Select)
 app.component('InputText', InputText)
+app.component('InputNumber', InputNumber)
 app.component('Dialog', Dialog)
 app.component('Rating', Rating)
-
+app.component('Panel', Panel)
+app.component('RadioButton', RadioButton)
 
 app.mount('#app')
