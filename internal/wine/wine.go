f31efffff2e6a95dee06e2b2d2e8c04ecb14cd4e
Beadko <beadko@gmail.com>
2024-08-18 17:12:56 +0700

    Update the Alcohol type to allow sending empty values

--- internal/wine/wine.go
+++ internal/wine/wine.go
@@ -8,6 +8,10 @@ type SafeInt struct {
 	zero.Int
 }
 
+type SafeFloat struct {
+	zero.Float
+}
+
 func (i *SafeInt) UnmarshalJSON(b []byte) error {
 	if string(b) == `""` {
 		i.Int = zero.IntFrom(0)
@@ -16,28 +20,36 @@ func (i *SafeInt) UnmarshalJSON(b []byte) error {
 	return i.Int.UnmarshalJSON(b)
 }
 
+func (i *SafeFloat) UnmarshalJSON(b []byte) error {
+	if string(b) == `""` {
+		i.Float = zero.FloatFrom(0)
+		return nil
+	}
+	return i.Float.UnmarshalJSON(b)
+}
+
 type Wine struct {
-	ID          int        `json:"id"`
-	Name        string     `json:"name"`
-	TypeID      SafeInt    `json:"wine_type,omitempty"`
-	CountryID   SafeInt    `json:"country,omitempty"`
-	Score       SafeInt    `json:"score,omitempty"`
-	Producer    string     `json:"producer,omitempty"`
-	Year        SafeInt    `json:"year,omitempty"`
-	Alcohol     zero.Float `json:"alcohol,omitempty"`
-	ColourID    SafeInt    `json:"colour,omitempty"`
-	DepthID     SafeInt    `json:"colour_depth,omitempty"`
-	ClarityID   SafeInt    `json:"clarity,omitempty"`
-	AromaID     SafeInt    `json:"aroma,omitempty"`
-	IntensityID SafeInt    `json:"intensity,omitempty"`
-	FlavourID   SafeInt    `json:"flavour,omitempty"`
-	SweetnessID SafeInt    `json:"sweetness,omitempty"`
-	AcidityID   SafeInt    `json:"acidity,omitempty"`
-	TanninID    SafeInt    `json:"tannin,omitempty"`
-	BodyID      SafeInt    `json:"body,omitempty"`
-	FinishID    SafeInt    `json:"finish,omitempty"`
-	BalanceID   SafeInt    `json:"balance,omitempty"`
-	Notes       string     `json:"notes,omitempty"`
+	ID          int       `json:"id"`
+	Name        string    `json:"name"`
+	TypeID      SafeInt   `json:"wine_type,omitempty"`
+	CountryID   SafeInt   `json:"country,omitempty"`
+	Score       SafeInt   `json:"score,omitempty"`
+	Producer    string    `json:"producer,omitempty"`
+	Year        SafeInt   `json:"year,omitempty"`
+	Alcohol     SafeFloat `json:"alcohol,omitempty"`
+	ColourID    SafeInt   `json:"colour,omitempty"`
+	DepthID     SafeInt   `json:"colour_depth,omitempty"`
+	ClarityID   SafeInt   `json:"clarity,omitempty"`
+	AromaID     SafeInt   `json:"aroma,omitempty"`
+	IntensityID SafeInt   `json:"intensity,omitempty"`
+	FlavourID   SafeInt   `json:"flavour,omitempty"`
+	SweetnessID SafeInt   `json:"sweetness,omitempty"`
+	AcidityID   SafeInt   `json:"acidity,omitempty"`
+	TanninID    SafeInt   `json:"tannin,omitempty"`
+	BodyID      SafeInt   `json:"body,omitempty"`
+	FinishID    SafeInt   `json:"finish,omitempty"`
+	BalanceID   SafeInt   `json:"balance,omitempty"`
+	Notes       string    `json:"notes,omitempty"`
 }
 type WineType struct {
 	ID   int    `json:"id"`
