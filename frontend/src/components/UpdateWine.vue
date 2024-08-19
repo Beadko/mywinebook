28b4f30e39322ffb73da0aea20a38c802f104d56
Beadko <beadko@gmail.com>
2024-08-19 11:00:59 +0700

    Ensure data is not updated until updateWine method is called

--- frontend/src/components/UpdateWine.vue
+++ frontend/src/components/UpdateWine.vue
@@ -14,28 +14,37 @@ export default {
     },
     data() {
         return {
-            tempScore: this.selected.score
-        };
+            temp_selected: {}
+        }
+    },
+    watch: {
+        selected: {
+            immediate: true,
+            handler(v) {
+                if (v) {
+                    this.temp_selected = { ...v };
+                }
+            }
+        }
     },
     methods: {
-    updateWine() {
-        this.selected.score = this.tempScore;
-        axios.put("/wine/"+ this.selected.id, this.selected)
-        .then(
-            this.update_dialog = false,
-            location.reload()
-        )
-        .catch((error) => {
-            window.alert(`The API returned an error: ${error}`)
-        })
-    },
-    onCountryAdded() {
-        this.$emit('country-added');
+        updateWine() {
+            Object.assign(this.selected, this.temp_selected);
+            axios.put("/wine/"+ this.selected.id, this.selected)
+            .then(
+                this.$parent.wine_dialog = false,
+            )
+            .catch((error) => {
+                window.alert(`The API returned an error: ${error}`)
+            })
         },
-    onTypeAdded() {
-        this.$emit('type-added');
-        }
-    }
+        onCountryAdded() {
+            this.$emit('country-added')
+        },
+        onTypeAdded() {
+            this.$emit('type-added')
+        },
+    },
 }
 </script>
 
@@ -43,21 +52,21 @@ export default {
     <Dialog :style="{ width: '25rem' }" header="Wine Details" modal>
         <div class="flex items-center gap-2 mb-4">
             <label for="name" class="font-semibold w-24">Name</label>
-            <InputText id="name" v-model="selected.name" class="w-full md:w-[14rem]" autocomplete="off"/>
+            <InputText id="name" v-model="temp_selected.name" class="w-full md:w-[14rem]" autocomplete="off"/>
         </div>
-        <WineTypes :selected="selected" @type-added="onTypeAdded" />
-        <Countries :selected="selected" @country-added="onCountryAdded"/>
+        <WineTypes :selected="temp_selected" @type-added="onTypeAdded" />
+        <Countries :selected="temp_selected" @country-added="onCountryAdded"/>
         <div class="flex items-center gap-2 mb-4">
             <label for="score" class="font-semibold w-24">Score</label>
-            <Rating v-model="tempScore" />
+            <Rating v-model="temp_selected.score" />
         </div>
         <div class="flex items-center gap-2 mb-3">
             <label for="year" class="font-semibold w-24">Year</label>
-                <InputNumber v-model="selected.year" inputId="withoutgrouping" :useGrouping="false" fluidclass="w-full md:w-[8rem]" />
+                <InputNumber v-model="temp_selected.year" inputId="withoutgrouping" :useGrouping="false" fluid class="w-full md:w-[8rem]" />
         </div>
         <div class="flex items-center gap-2 mb-3">
             <label for="alcohol" class="font-semibold w-24">Alcohol</label>
-            <InputNumber v-model="selected.alcohol" inputId="decimal" :minFractionDigits="1" suffix="%" class="w-full md:w-[8rem]" />
+            <InputNumber v-model="temp_selected.alcohol" inputId="decimal" :minFractionDigits="1" suffix="%" class="w-full md:w-[8rem]" />
         </div>        
         <template #footer>
             <Button label="Cancel" icon="pi pi-times" text @click="this.$parent.wine_dialog = false" />
