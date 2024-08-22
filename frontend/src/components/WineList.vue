028b03c4b69ed12f991b33de79a0a37ea91dd462
Beadko <beadko@gmail.com>
2024-08-22 17:25:09 +0700

    Combine UpdateWine and AddWine, display additional properties in the WineList

--- frontend/src/components/WineList.vue
+++ frontend/src/components/WineList.vue
@@ -1,16 +1,14 @@
 <script>
-import axios from 'axios'
-import AddWine from './AddWine.vue'
-import DeleteWine from './DeleteWine.vue'
-import UpdateWine from './UpdateWine.vue'
-import { store } from './store'
+import axios from 'axios';
+import WineForm from './WineForm.vue';
+import DeleteWine from './DeleteWine.vue';
+import { store } from './store';
 
 export default {
     name: "WineList",
     components: {
-        AddWine,
-        DeleteWine,
-        UpdateWine
+        WineForm,
+        DeleteWine
     },
     data() {
         return {
@@ -20,14 +18,15 @@ export default {
             selected: {},
             delete_dialog: false,
             wine_dialog: false,
+            form_mode: 'add',
         }
     },
     computed: {
-        wineTypeMap: function() {
-            return this.store.wine_types.reduce((acc,cur)=>{acc[cur.id]=cur; return acc},{})
+        wineTypeMap() {
+            return this.store.wine_types.reduce((acc, cur) => { acc[cur.id] = cur; return acc }, {});
         },
-        countryMap: function() {
-            return this.store.countries.reduce((acc,cur)=>{acc[cur.id]=cur; return acc},{})
+        countryMap() {
+            return this.store.countries.reduce((acc, cur) => { acc[cur.id] = cur; return acc }, {});
         },
     },
     methods: {
@@ -40,64 +39,85 @@ export default {
         },
         getWines() {
             axios.get("/wine")
-            .then(res => {
-                this.wines =res.data
-            })
-            .catch((error) => {
-                window.alert(`The API returned an error: ${error}`)
-            })
+                .then(res => {
+                    this.wines = res.data;
+                })
+                .catch((error) => {
+                    window.alert(`The API returned an error: ${error}`);
+                });
         },
         getWineTypes() {
             axios.get("/wine_type")
-            .then(res => {
-                this.store.wine_types = res.data
-            })
-            .catch((error) => {
-                window.alert(`The API returned an error: ${error}`);
-            })
+                .then(res => {
+                    this.store.wine_types = res.data;
+                })
+                .catch((error) => {
+                    window.alert(`The API returned an error: ${error}`);
+                });
         },
         getCountries() {
             axios.get("/country")
-            .then(res => {
-                this.store.countries = res.data
-            })
-            .catch((error) => {
-                window.alert(`The API returned an error: ${error}`);
-            })
+                .then(res => {
+                    this.store.countries = res.data;
+                })
+                .catch((error) => {
+                    window.alert(`The API returned an error: ${error}`);
+                });
         },
         selectWine(wn) {
-            this.selected = wn
-            this.wine_dialog = true
+            this.selected = wn;
+            this.form_mode = 'update';
+            this.wine_dialog = true;
         },
         deleteWine(wn) {
-            this.selected = wn
-            this.delete_dialog = true
+            this.selected = wn;
+            this.delete_dialog = true;
         },
-        getWineTypeName(wine){
-            return this.wineTypeMap[wine.data.wine_type]?.name || 'Unknown'
+        getWineTypeName(wine) {
+            return this.wineTypeMap[wine.data.wine_type]?.name || 'Unknown';
         },
-        getCountryName(wine){
-            return this.countryMap[wine.data.country]?.name || 'Unknown'
+        getCountryName(wine) {
+            return this.countryMap[wine.data.country]?.name || 'Unknown';
         },
         removeWine(wn) {
-            this.wines = this.wines.filter(wine => wine.id !== wn)
+            this.wines = this.wines.filter(wine => wine.id !== wn);
         },
         addNewWine(wn) {
-            this.wines.push(wn)
+            this.wines.push(wn);
+        },
+        openAddWineForm() {
+            this.selected = {
+                name: '',
+                wine_type: '',
+                country: '',
+                score: '',
+                producer: '',
+                year: null,
+                alcohol: null,
+            };
+            this.form_mode = 'add';
+            this.wine_dialog = true;
+        },
+        handleWineAdded(wn) {
+            this.addNewWine(wn);
+            this.wine_dialog = false;
+        },
+        handleWineUpdated(wn) {
+            this.wine_dialog = false;
         },
     },
     async mounted() {
-        this.getCountries()
-        this.getWineTypes()
-        this.getWines()
+        this.getCountries();
+        this.getWineTypes();
+        this.getWines();
     }
 }
 </script>
 
 <template>
     <h1> Your Wine List</h1>
-    <AddWine :wines="wines" @wine-added="addNewWine"/> 
-    <DataTable v-model:expandedRows="expanded_row" :value="wines" dataKey="id"   tableStyle="min-width: 60rem" @row-click="ExpandRow($event.data)">
+    <Button label="+ Add Wine" @click="openAddWineForm" class="m-8"/>
+    <DataTable v-model:expandedRows="expanded_row" :value="wines" dataKey="id" tableStyle="min-width: 60rem" @row-click="ExpandRow($event.data)">
         <Column headerStyle="width:4rem">
             <template #body="wine">
                 <i class="pi pi-chevron-right" style="color: #708090" v-if="!expanded_row[wine.data.id]" />
@@ -107,17 +127,17 @@ export default {
         <Column field="name" header="Name" />
         <Column field="wine_type" header="Type">
             <template #body="wine">
-                {{ getWineTypeName(wine)}}
+                {{ getWineTypeName(wine) }}
             </template>
         </Column>
         <Column field="country" header="Country">
             <template #body="wine">
-                {{ getCountryName(wine)}}
+                {{ getCountryName(wine) }}
             </template>
         </Column>
         <Column field="score" header="Score">
             <template #body="wine">
-                <Rating v-model="wine.data.score" readonly/>
+                <Rating v-model="wine.data.score" readonly />
             </template>
         </Column>
         <Column headerStyle="width:4rem">
@@ -127,12 +147,19 @@ export default {
             </template>
         </Column>
         <template #expansion="slotProps">
-            <div class="p-4">
-                <p>Producer: {{ slotProps.data.producer }}</p>
-                <p>Year: {{ slotProps.data.year }}</p>
+            <div class="flex items-center p-4 gap-8">
+                <div lass="flex items-center">
+                    <div class="font-medium mr-2">Producer:</div> {{ slotProps.data.producer }}
+                </div>
+                <div class="flex items-center">
+                    <div class="font-medium mr-2">Year:</div> {{ slotProps.data.year }}
+                </div>
+                <div class="flex items-center">
+                    <div class="font-medium mr-2">Alcohol:</div> {{ slotProps.data.alcohol }}%
+                </div>
             </div>
         </template>
     </DataTable>
-    <DeleteWine v-model:visible="delete_dialog" :selected="selected" @wine_deleted="removeWine"/>
-    <UpdateWine v-model:visible="wine_dialog" :selected="selected" />
-</template> 
+    <DeleteWine v-model:visible="delete_dialog" :selected="selected" @wine_deleted="removeWine" />
+    <WineForm v-model:visible="wine_dialog" :selected="selected" :mode="form_mode" @wine_added="handleWineAdded" @wine_updated="handleWineUpdated" />
+</template>
